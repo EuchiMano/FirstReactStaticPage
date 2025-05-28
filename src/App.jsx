d5b7eeddfdd453ac0d0219e497afc995ed2d2f32
EuchiMano <marce.ps666@gmail.com>
2025-05-28 01:37:23 -0400

    fix conflicts

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/navbar";
+import Main from "../components/main";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
+}
